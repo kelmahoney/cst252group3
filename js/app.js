3871f04c92b8b1e276af0086ebc56db46f1e7e5e
kelmahoney <kelmahoneya@gmail.com>
2022-05-15 21:54:30 -0700

    idk

--- js/app.js
+++ js/app.js
@@ -1,141 +1,68 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
+// @path-json can be an object or an array, the first will be loaded directly, the object from the array will be random selected
+/* tsParticles.loadJSON(@dom-id, @path-json, @callback (optional)); */
 
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
+tsParticles
+    .loadJSON("tsparticles", "presets/default.json")
+    .then((container) => {
+        console.log("callback - tsparticles config loaded");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+//or
+
+/* tsParticles.load(@dom-id, @options); */
+
+tsParticles.load("tsparticles", {
+    /* options here */
 });
-*/
 
-/* Otherwise just put the config content (json): */
+//or
 
-particlesJS('particles-js',
+/* tsParticles.loadFromArray(@dom-id, @options, @index (optional)); */
 
-  {
-    "particles": {
-      "number": {
-        "value": 20,
-        "density": {
-          "enable": true,
-          "value_area": 800
-
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "image",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/bottle.png",
-          "width": 100,
-          "height": 100
-
-        },
-        "image1": {
-          "src": "img/bottle.png",
-          "width": 100,
-          "height": 100
-
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 60,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 20,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
+tsParticles.loadFromArray("tsparticles", [
+    {
+        /* options here */
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
+    {
+        /* other options here */
     },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
+]);
+//random object
 
-);
+tsParticles.loadFromArray(
+    "tsparticles",
+    [
+        {
+            /* options here */
+        },
+        {
+            /* other options here */
+        },
+    ],
+    1
+); //the second one
+// Important! If the index is not in range 0...<array.length, the index will be ignored.
+
+// after initialization this can be used.
+
+/* tsParticles.setOnClickHandler(@callback); */
+
+/* this will be fired from all particles loaded */
+
+tsParticles.setOnClickHandler((event, particles) => {
+    /* custom on click handler */
+});
+
+// now you can control the animations too, it's possible to pause and resume the animations
+// these methods don't change the config so you're safe with all your configurations
+// domItem(0) returns the first tsParticles instance loaded in the dom
+const particles = tsParticles.domItem(0);
+
+// play will start the animations, if the move is not enabled it won't enable it, it just updates the frame
+particles.play();
+
+// pause will stop the animations
+particles.pause();
